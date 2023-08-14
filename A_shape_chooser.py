8a6382c5146cbd8c5df03f3179eb855d472232d4
wiggleywobbley <110356499+wiggleywobbley@users.noreply.github.com>
2023-08-14 22:11:36 +0000

    commented shape chooser and area perimeter

--- A_shape_chooser.py
+++ A_shape_chooser.py
@@ -1,4 +1,6 @@
 def shape():
+  #Ask what shape the user wants the area/perimeter calculated for 
   shape = input("What Shape do you want the area/perimeter calculated? \
   Circle, Square, Rectangle, Triangle, Parellelogram")
+  #return user input
   return shape
--- B_area_perimeter.py
+++ B_area_perimeter.py
@@ -1,4 +1,6 @@
 def area_per():
+  #ask user whether they would like the area or perimeter calculated
   area_per = input("What Shape do you want the area/perimeter calculated? \
   Circle, Square, Rectangle, Triangle, Parellelogram")
+  #return user input
   return area_per
